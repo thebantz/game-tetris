b2c88f9a8d6df2e6d867c4e2ee97f4f995de00b4
Nicole <nicoledbanta@gmail.com>
2020-05-18 22:32:12 -0700

    Enable freeze functionality for Tetrominoes to stop moving down

--- app.js
+++ app.js
@@ -43,18 +43,45 @@ document.addEventListener('DOMContentLoaded', () => {
   const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
 
   let currentPosition = 4;
+  let currentRotation = 0;
   let random = Math.floor(Math.random() * theTetrominoes.length);
-  let current = theTetrominoes[random][0];
+  let current = theTetrominoes[random][currentRotation];
 
 
-  // draw the first tetromino
+  // draw & undraw the tetrominoes
 
   const draw = () => {
     current.forEach(index => {
       squares[currentPosition + index].classList.add('tetromino');
-    })
+    });
+  };
+
+  const undraw = () => {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.remove('tetromino');
+    });
+  };
+
+  // make the tetrominoes move down per second
+  const freeze = () => {
+    if (current.some(index => 
+      squares[currentPosition + index + width].classList.contains('taken')
+    )) {
+      current.forEach(index => squares[currentPosition + index].classList.add('taken'));
+      random = Math.floor(Math.random() * theTetrominoes.length);
+      current = theTetrominoes[random][currentRotation];
+      currentPosition = 4;
+      draw();
+    }
   }
 
-  draw();
 
+  const moveDown = () => {
+    undraw();
+    currentPosition += width;
+    draw();
+    freeze();
+  };
+
+  // timerId = setInterval(moveDown, 100);
 });
