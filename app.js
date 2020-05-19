332bc000a169582f9642f9a437cf89b69323c39c
Nicole <nicoledbanta@gmail.com>
2020-05-18 21:29:38 -0700

    Add a 200 cell grid with styling

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+  const grid = document.querySelector('.grid');
+  let squares = Array.from(document.querySelectorAll('.grid div'));
+  const width = 10;
+  const ScoreDisplay = document.querySelector('#score');
+  const StartBtn = document.querySelector('#start-button');
+  
 });
