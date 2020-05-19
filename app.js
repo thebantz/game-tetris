48901b6319a8c360a14451747166b984ba409292
Nicole <nicoledbanta@gmail.com>
2020-05-18 22:07:10 -0700

    Render tetrominoes on screen

--- app.js
+++ app.js
@@ -4,5 +4,57 @@ document.addEventListener('DOMContentLoaded', () => {
   const width = 10;
   const ScoreDisplay = document.querySelector('#score');
   const StartBtn = document.querySelector('#start-button');
-  
+
+  const lTetromino = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2]
+  ];
+
+  const zTetromino = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1]
+  ];
+
+  const tTetromino = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1]
+  ];
+
+  const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1]
+  ];
+
+  const iTetromino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3]
+  ];
+
+  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+
+  let currentPosition = 4;
+  let random = Math.floor(Math.random() * theTetrominoes.length);
+  let current = theTetrominoes[random][0];
+
+
+  // draw the first tetromino
+
+  const draw = () => {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.add('tetromino');
+    })
+  }
+
+  draw();
+
 });
